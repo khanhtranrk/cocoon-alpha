3a5b8a8022c323717873841675e7970b3819a4a1
Khanh Tran <khanhtran.rk@gmail.com>
2024-09-12 16:53:22 +0700

    init

--- external/cocoon/db.go
+++ external/cocoon/db.go
@@ -2,12 +2,11 @@ package cocoon
 
 import (
 	"database/sql"
-	"github.com/khanhtranrk/justice-alpha/external/adapter/config"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func NewDb(config *config.DB) (*sql.DB, error) {
-  db, err := sql.Open("sqlite3", config.DatabaseURL)
+func NewDb(config *Config) (*sql.DB, error) {
+  db, err := sql.Open("sqlite3", config.DatabaseUrl)
 
   if err != nil {
     return nil, err
